0f94727994910758a14b7240ca34a5c29ce6898c
Paul Jeon <pauljeonn@gmail.com>
2021-11-04 23:03:41 +0900

    handle login auth

--- routes/auth.js
+++ routes/auth.js
@@ -14,8 +14,34 @@ router.post('/register', async (req, res) => {
 	});
 
 	try {
+		// use await since it may take some time to save
 		const savedUser = await newUser.save();
 		res.status(201).json(savedUser);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+	try {
+		const user = await User.findOne({ username: req.body.username });
+		!user && res.status(401).json('Wrong credentials!');
+
+		// decrypt the password
+		const hashedPassword = CryptoJS.AES.decrypt(
+			user.password,
+			process.env.PASS_SECRET
+		);
+
+		// compare password then return user data if they match
+		const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+		originalPassword !== req.body.password &&
+			res.status(401).json('Wrong credentials!');
+
+		// return user data except the password
+		const { password, ...others } = user._doc;
+		res.status(200).json(others);
 	} catch (err) {
 		res.status(500).json(err);
 	}
