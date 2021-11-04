1a0cb211a0f6328087e5a2ed7dcd73c570e401ed
Paul Jeon <pauljeonn@gmail.com>
2021-11-04 19:03:42 +0900

    ecommerce api with routes and models

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('DB Connection Successful!'))
+	.catch((err) => console.log(err));
+
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+
+app.listen(process.env.PORT || 4000, () => {
+	console.log('Backend server is running!');
+});
