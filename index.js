9bc34a589d14816d05bcb53d6d03aa87891e6038
Paul Jeon <pauljeonn@gmail.com>
2021-11-09 03:25:13 +0900

    CRUD for oder.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,7 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 4000, () => {
 	console.log('Backend server is running!');
